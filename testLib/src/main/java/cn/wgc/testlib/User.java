70532bd9c28676e8f20e49072f464296323e5940
wangguangcheng <ts_android_050@dabby.cn>
2022-06-29 17:24:58 +0800

    初始化提交

---
+++ testLib/src/main/java/cn/wgc/testlib/User.java
@@ -0,0 +1,33 @@
+package cn.wgc.testlib;
+
+/**
+ * <pre>
+ *
+ *     author : wgc
+ *     time   : 2022/06/29
+ *     desc   :
+ *     version: 1.0
+ *
+ * </pre>
+ */
+public class User {
+
+  private String name;
+  private String idNum;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIdNum() {
+    return idNum;
+  }
+
+  public void setIdNum(String idNum) {
+    this.idNum = idNum;
+  }
+}
